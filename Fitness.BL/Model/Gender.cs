611538dec440a6bb6d0032d169f6adb3b6a06fc0
NeProstoRafik <Mr.Blaki@mail.ru>
2022-12-13 17:37:44 +0500

    Создана модель пользователя.

---
+++ Fitness.BL/Model/Gender.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fitness.BL.Model
+{
+    public class Gender
+    {
+        public string Name { get;}
+        public Gender(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("Не может быть пустым или налл", nameof(name));
+            }
+            Name = name;
+        }
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
